773cb2b6d5cc417a6581991cad3ac61c33fa56dc
alexdm1998 <62345984+alexdm1998@users.noreply.github.com>
2021-05-11 14:17:55 +0100

    Last commit

--- src/app/my-bar-chart/my-bar-chart.component.ts
+++ src/app/my-bar-chart/my-bar-chart.component.ts
@@ -19,12 +19,9 @@ export class MyBarChartComponent implements OnInit {
   constructor(private _graph_data:GraphDataService) {
     this._graph_data.messageChanges$.subscribe((msg: object)=>{
       this.Dataflow = msg['data'];
-      console.log(this.chart);
       if(this.chart !== undefined){
         this.chart.destroy()
-        console.log("Chart destruido");
       }
-      console.log(this.chart);
       this.InitDataflow()
     })
   }
@@ -36,6 +33,9 @@ export class MyBarChartComponent implements OnInit {
       let uniqueTIME_PERIOD = TIME_PERIOD.filter((c, index) => {
         return TIME_PERIOD.indexOf(c) === index;
       });
+      console.log(uniqueTIME_PERIOD)
+      uniqueTIME_PERIOD.sort()
+      console.log(uniqueTIME_PERIOD)
       let datasetsFinal = {};
       OBS_VALUE.filter((data)=>{
         if(datasetsFinal[`${data[3]}`] == undefined){
@@ -44,19 +44,13 @@ export class MyBarChartComponent implements OnInit {
           datasetsFinal[`${data[3]}`].push([data[0], data[2]])
         }
       })
-      /* console.log(datasetsFinal);
-      console.log(Object.keys(datasetsFinal).length)
-      console.log(Object.keys(datasetsFinal));
-      console.log(datasetsFinal[Object.keys(datasetsFinal)[5]])
-      console.log(Object.keys(datasetsFinal)[0]); */
+
       let FinalArray = [];
       for(var i = 0; i < Object.keys(datasetsFinal).length; i++){
         let templateDataset = {};
         for(let TimePeriod in uniqueTIME_PERIOD){
           if(templateDataset['label'] == undefined){templateDataset['label'] = Object.keys(datasetsFinal)[i]}
           if(templateDataset['data'] == undefined){templateDataset['data'] = []}
-          console.log(datasetsFinal[Object.keys(datasetsFinal)[i]][0][0]);
-          console.log(TimePeriod);
           if(datasetsFinal[Object.keys(datasetsFinal)[i]][0][0] == uniqueTIME_PERIOD[TimePeriod]){
             templateDataset['data'].push(datasetsFinal[Object.keys(datasetsFinal)[i]][0][1])
           }else{
@@ -66,6 +60,7 @@ export class MyBarChartComponent implements OnInit {
         FinalArray.push(templateDataset);
       }
       console.log(FinalArray);
+
       this.chart = new Chart('canvas', {
         type: 'bar',
         data: {
